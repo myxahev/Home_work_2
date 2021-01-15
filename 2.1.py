708487767431448dcafee38d4760dce029867412
Dmitry <myxahev2@gmail.com>
2021-01-15 17:04:12 +0500

    complete 2.1

---
+++ 2.1.py
@@ -0,0 +1,8 @@
+
+result_list = [2, 'text', 456, 45.3, None]
+
+print(type(result_list))
+print(type(result_list[1]))
+print(type(result_list[2]))
+print(type(result_list[3]))
+print(type(result_list[4]))
