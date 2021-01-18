283f557ed77d560de4d3df9b67f29538fd48f901
myxahev <76581169+myxahev@users.noreply.github.com>
2021-01-18 22:09:43 +0500

    update 2.1

--- 2.1.py
+++ 2.1.py
@@ -1,8 +1,4 @@
 
 result_list = [2, 'text', 456, 45.3, None]
-
-print(type(result_list))
-print(type(result_list[1]))
-print(type(result_list[2]))
-print(type(result_list[3]))
-print(type(result_list[4]))
+for i in result_list:
+  print(type(i))
